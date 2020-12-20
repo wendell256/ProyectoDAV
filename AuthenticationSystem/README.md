2af9ff80f85aea3fa9a3a2f574bdd899667a9f4f
andreajmc <andreaj@unitec.edu>
2020-12-20 02:11:10 -0600

    Authentication System 100% funcional
    aka llevamos 10 puntos

---
+++ AuthenticationSystem/README.md
@@ -0,0 +1,22 @@
+# loginreg-global
+Login and Registration App with MongoDB Atlas
+
+## Visit Here
+https://login-register-hmv.herokuapp.com/
+
+## Quick Start
+
+```bash
+# Install dependencies for server
+npm install
+```
+
+## Note (In case, the app is not deployed)
+
+You will need to create a keys_dev.js in the server config folder with
+
+```
+module.exports = {
+  MongoURI: 'YOUR_OWN_MONGO_URI',
+};
+```
---
+++ AuthenticationSystem/model/levels.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// Schema
+
+var LevelSchema = mongoose.Schema({
+    name: {
+        type: String,
+        index: true
+    },
+    content: {
+        type: String,
+    },
+
+});
+
+var Level = module.exports = mongoose.model('Level', LevelSchema);
+
+
+module.exports.getLevel = function (levelname, callback) {
+    var query = { username: username };
+    Level.findOne(query, callback);
+}
+
+
