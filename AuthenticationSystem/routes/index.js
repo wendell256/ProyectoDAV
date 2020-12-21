cea8b46c7e7d886c3bc9be418c66810a4b9fd054
wendell256 <wendell256@gmail.com>
2020-12-21 00:24:06 -0600

    lesgo ya

--- AuthenticationSystem/routes/index.js
+++ AuthenticationSystem/routes/index.js
@@ -1,11 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+var levels = [];
+//var Level = require('../model/levels')
 
-var levelPlans = [];
-// Get Home
+
 router.get('/', ensureAuthentication, function (req, res) {
+    var level1;
+    var level2;
+    var level3;
+    const dbName = 'levels';
 
-    res.render('index');
+    MongoClient.connect("mongodb+srv://andrea:bananas@cluster0.rbcj8.mongodb.net/users?retryWrites=true&w=majority", (err, client) => {
+        if (err) { console.log("error") }
+
+        let db = client.db(dbName);
+        var collection = db.collection('levels');
+        collection.findOne({ name: "1" }, function (err, item) { level1 = item.content; })
+        collection.findOne({ name: "2" }, function (err, item) { level2 = item.content; })
+        collection.findOne({ name: "3" }, function (err, item) { level3 = item.content; })
+        collection.findOne({ name: "3" }, function (err, item) { res.render('index', { lvl1: level1, lvl2: level2, lvl3: level3 }); })
+
+    });
+
+
+
+
+
+    /*
+    Level.getLevel("1", function (err, level) {
+        console.log("lvl");
+        console.log(level.name);
+        /*var res = level.name.split("-");
+        levels.push(res);
+    });
+    Level.getLevel("2", function (err, level) {
+        /*var res = level.name.split("-");
+        levels.push(res);
+    });
+    Level.getLevel("3", function (err, level) {
+        /*var res = level.name.split("-");
+        levels.push(res);
+    });*/
+
 });
 
 
